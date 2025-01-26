0098b8751d15161a444371d6bb4af63b13d46549
Brian Park <park-brian@users.noreply.github.com>
2025-01-25 19:21:27 -0500

    create solid app

--- client/main.js
+++
@@ -1,6 +0,0 @@
-import { h, render } from "preact";
-import htm from "htm";
-import App from "./components/app.js";
-
-const html = htm.bind(h);
-render(html`<${App} />`, window.app);
---
+++ client/pages/projects/page.js
@@ -0,0 +1,5 @@
+import html from "solid-js/html";
+
+export default function Projects() {
+  return html`<div>Projects page</div>`;
+}
