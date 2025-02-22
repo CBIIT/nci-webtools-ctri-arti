057225bbd0a180fc4dd2a7c36df32472dd863a3c
Brian Park <park-brian@users.noreply.github.com>
2025-02-22 13:52:50 -0500

    don't cache static resources

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ import api from "./services/api.js";
 const { PORT = 8080, CLIENT_FOLDER = "../client" } = process.env;
 const app = express();
 
-app.use(express.static(CLIENT_FOLDER));
+const setHeaders = (res) => res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+app.use(express.static(CLIENT_FOLDER, { setHeaders }));
 app.use("/api", api);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
