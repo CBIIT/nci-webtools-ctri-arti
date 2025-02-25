55295908cfa9374a6f3ed38c81f0d14dba5443a2
Brian Park <park-brian@users.noreply.github.com>
2025-02-25 00:13:48 -0500

    replace dash

--- client/agents/compliai/index.js
+++ client/agents/compliai/index.js
@@ -488,7 +488,7 @@ export default function Page() {
           </div>
 
           <select class="form-select form-select-sm border-0 bg-transparent cursor-pointer" name="model" id="model" required>
-            <option value="us.anthropic.claude-3-7-sonnet-20250219-v1:O" selected>Sonnet</option>
+            <option value="us.anthropic.claude-3-7-sonnet-20250219-v1:0" selected>Sonnet</option>
             <option value="us.anthropic.claude-3-5-haiku-20241022-v1:0">Haiku</option>
           </select>
 
