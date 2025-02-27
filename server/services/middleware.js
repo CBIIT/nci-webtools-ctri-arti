09c500d5e5ffd1d827f710ec6d0553256dc43e56
Brian Park <park-brian@users.noreply.github.com>
2025-02-27 18:43:49 -0500

    only allow .mil and .gov domains

--- server/services/middleware.js
+++ server/services/middleware.js
@@ -1,8 +1,15 @@
 import { Readable } from "stream";
 
+export const WHITELIST = [/\.gov$/i, /\.mil$/i];
+
 export async function proxyMiddleware(req, res) {
   const { headers, method, body, query } = req;
   const url = new URL(query.url ?? body?.url ?? "");
+  if (!WHITELIST.some((regex) => regex.test(url.hostname))) {
+    res.statusCode = 403;
+    res.end(`Forbidden: Only the following domain patterns are allowed: ${WHITELIST.map((r) => r.source).join(", ")}`);
+    return
+  }
   const badRequestHeaders = ["host", "connection", "content-length"];
   const badResponseHeaders = ["content-encoding", "content-length"];
   badRequestHeaders.forEach((h) => delete headers[h]);
