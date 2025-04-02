977cd4662772f0183fe589a5a7950cd45ecaa355
Brian Park <park-brian@users.noreply.github.com>
2025-04-02 19:40:15 -0400

    disable message cache

--- server/services/inference.js
+++ server/services/inference.js
@@ -47,7 +47,7 @@ export async function runModel(
       }
     }
   }
-  messages.at(-1).content.push({ cachePoint });
+  // messages.at(-1).content.push({ cachePoint });
 
   const client = new BedrockRuntimeClient();
   const system = [{ text: systemPrompt }, { cachePoint }];
