c2a66edd7cf8fb2b41d96b6a9a3bcec0ded7b8f7
Brian Park <park-brian@users.noreply.github.com>
2025-04-07 14:16:02 -0400

    add translation service

---
+++ server/services/translate.js
@@ -0,0 +1,30 @@
+import { TranslateClient, TranslateTextCommand, paginateListLanguages } from "@aws-sdk/client-translate"; // ES Modules import
+
+export async function translate(text, sourceLanguage = "en", targetLanguage = "es", settings = {}) {
+    const client = new TranslateClient();
+    const input = {
+        Text: text,
+        SourceLanguageCode: sourceLanguage,
+        TargetLanguageCode: targetLanguage,
+        Settings: {
+            Formality: "FORMAL",
+            Profanity: "MASK",
+            ...settings
+        },
+    };
+    const command = new TranslateTextCommand(input);
+    const response = await client.send(command);
+    return response.TranslatedText;
+}
+
+export async function getLanguages() {
+    const client = new TranslateClient();
+    const paginator = paginateListLanguages({ client }, {});
+    const languages = [];
+    for await (const page of paginator) {
+        if (page.Languages) {
+            languages.push(...page.Languages);
+        }
+    }
+    return languages;
+}
