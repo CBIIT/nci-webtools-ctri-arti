3c6b5cbeba629419dd86df0a58f77159272a6238
Brian Park <park-brian@users.noreply.github.com>
2025-04-09 18:57:49 -0400

    set default rotation speed

--- client/components/dna-spinner.js
+++ client/components/dna-spinner.js
@@ -2,4 +2,4 @@ import html from "solid-js/html";
 import customElement from "../agents/utils/custom-element.js";
 import DNASpinner from "../agents/components/dna-spinner.js";
 
-customElement("dna-spinner", () => html`<${DNASpinner} rotationSpeed=${0.0005} />`);
+customElement("dna-spinner", { rotationSpeed: 0.0001 }, (props) => html`<${DNASpinner} ...${props} />`);
