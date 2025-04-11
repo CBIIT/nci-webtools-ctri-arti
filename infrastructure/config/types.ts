a0fa763e1f8881a9bdf99e9420520ca6b3d45083
Brian Park <park-brian@users.noreply.github.com>
2025-04-11 18:39:34 -0400

    update rds type

--- infrastructure/config/types.ts
+++ infrastructure/config/types.ts
@@ -1,18 +1,18 @@
-import { EcrRepositoryStackProps } from '../lib/stacks/ecr-repository-stack';
-import { EcsServiceStackProps } from '../lib/stacks/ecs-service-stack';
-// import { EfsFilesystemStackProps } from '../lib/stacks/efs-filesystem-stack';
-import { RdsInstanceStackProps } from '../lib/stacks/rds-instance-stack';
-import { Route53HostedZoneStackProps } from '../lib/stacks/route53-hosted-zone-stack';
+import { EcrRepositoryStackProps } from "../lib/stacks/ecr-repository-stack";
+import { EcsServiceStackProps } from "../lib/stacks/ecs-service-stack";
+import { EfsFilesystemStackProps } from '../lib/stacks/efs-filesystem-stack';
+import { RdsClusterStackProps } from "../lib/stacks/rds-cluster-stack";
+import { Route53HostedZoneStackProps } from "../lib/stacks/route53-hosted-zone-stack";
 
 export interface Config {
   env: {
     account: string;
     region: string;
-  },
-  // route53: Route53HostedZoneStackProps,
+  };
+  route53?: Route53HostedZoneStackProps;
   ecr: EcrRepositoryStackProps;
   ecs: EcsServiceStackProps;
-  // efs: EfsFilesystemStackProps;
-  // rds: RdsInstanceStackProps;
+  efs?: EfsFilesystemStackProps;
+  rds: RdsClusterStackProps;
   tags: Record<string, string>;
 }
