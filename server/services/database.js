719d33c2b9967f876aaf72a3a42326d37753cb71
Brian Park <park-brian@users.noreply.github.com>
2025-04-11 19:08:44 -0400

    return outputs as json

--- server/services/database.js
+++ server/services/database.js
@@ -1,15 +1,19 @@
 import { RDSDataClient, ExecuteStatementCommand } from "@aws-sdk/client-rds-data";
+import { retry } from "./utils.js";
 
-export async function query(sql, env = process.env) {
+export async function query(sql, parameters = [],  env = process.env) {
   const client = new RDSDataClient();
   const params = {
     sql: sql,
+    parameters: parameters,
+    continueAfterTimeout: true,
     database: env.DATABASE,
     resourceArn: env.DATABASE_ARN,
     secretArn: env.DATABASE_SECRET_ARN,
+    formatRecordsAs: "JSON",
   }
 
   const command = new ExecuteStatementCommand(params);
-  const output = await client.send(command);
-  return output.records;
+  const output = await retry(() => client.send(command), 10, 100);
+  return JSON.parse(output.formattedRecords) || [];
 }
