ca027ffbb5806b67a4797e69374e9158c4f975d6
Brian Park <park-brian@users.noreply.github.com>
2025-04-19 14:35:12 -0400

    add routes

--- client/pages/routes.js
+++ client/pages/routes.js
@@ -3,8 +3,8 @@ import { lazy } from "solid-js";
 import Home from "./home.js";
 const Tools = () => import("./tools/index.js");
 const Agents = () => import("./agents/index.js");
-const Chat = () => import("./tools/chat.js");
-const FedPulse = () => import("./tools/fedpulse.js");
+const Chat = () => import("./agents/chat.js");
+const FedPulse = () => import("./agents/fedpulse.js");
 const Translate = () => import("./tools/translate.js");
 const ConsentCrafter = () => import("./tools/consent-crafter.js");
 const Workspaces = () => import("./tools/workspaces/index.js");
@@ -17,7 +17,19 @@ const routes = [
     hidden: false,
   },
   {
-    path: "/tools/fedpulse",
+    path: "/agents/policyai",
+    title: "PolicyAI",
+    component: lazy(FedPulse),
+    hidden: true,
+  },
+  {
+    path: "/agents/chat",
+    title: "Chat",
+    component: lazy(Chat),
+    hidden: true,
+  },
+  {
+    path: "/agents/fedpulse",
     title: "FedPulse",
     component: lazy(FedPulse),
   },
