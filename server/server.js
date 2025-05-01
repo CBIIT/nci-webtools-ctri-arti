83f040244287b1ba554c807cc7cf9a3f2ea3507f
Brian Park <park-brian@users.noreply.github.com>
2025-04-30 23:58:07 -0400

    simplify server.js

--- server/server.js
+++ server/server.js
@@ -6,32 +6,33 @@ import session from "express-session";
 import { createLogger } from "./services/logger.js";
 import api from "./services/api.js";
 
-const { PORT = 8080, CLIENT_FOLDER = "../client", HTTPS_PEM, SESSION_SECRET, LOG_LEVEL = "info" } = process.env;
+const { PORT = 8080 } = process.env;
 
-const app = express();
-app.set("trust proxy", true);
-app.locals.logger = createLogger("research-optimizer", LOG_LEVEL);
+const app = createApp(process.env);
+createServer(app, process.env).listen(PORT, () => app.locals.logger.info(`Server is running on port ${PORT}`));
 
-// set up session
-app.use(
-  session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    // cookie: { secure: true },
-    proxy: true,
-  })
-);
-// import api
-app.use("/api", api);
+export function createApp(env = process.env) {
+  const { CLIENT_FOLDER = "../client", SESSION_SECRET, LOG_LEVEL = "info" } = env;
 
-// static files
-const setHeaders = (res) => res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-app.use(express.static(CLIENT_FOLDER, { setHeaders }));
-app.get(/.*/, (req, res) => res.sendFile("index.html", { root: CLIENT_FOLDER }));
+  // create express app with logger, session, api
+  const app = express();
+  app.locals.logger = createLogger("research-optimizer", LOG_LEVEL);
+  app.set("trust proxy", true);
+  app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: true }));
+  app.use("/api", api);
 
-// http/https server
-const lib = HTTPS_PEM ? https : http;
-const cert = HTTPS_PEM ? readFileSync(HTTPS_PEM) : undefined;
-const options = { requestTimeout: 0, key: cert, cert };
-lib.createServer(options, app).listen(PORT, () => app.locals.logger.info(`Server is running on port ${PORT}`));
+  // serve uncached static files, with 404 fallback for index.html
+  const setHeaders = (res) => res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  app.use(express.static(CLIENT_FOLDER, { setHeaders }));
+  app.get(/.*/, (req, res) => res.sendFile("index.html", { root: CLIENT_FOLDER }));
+
+  return app;
+}
+
+export function createServer(app, env = process.env) {
+  const { HTTPS_PEM } = env;
+  const lib = HTTPS_PEM ? https : http;
+  const cert = HTTPS_PEM ? readFileSync(HTTPS_PEM) : undefined;
+  const options = { requestTimeout: 0, key: cert, cert };
+  return lib.createServer(options, app);
+}
