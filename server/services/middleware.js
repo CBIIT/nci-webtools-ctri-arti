57af4c8f14421a2e3af29898468ee165f1db7b9c
Brian Park <park-brian@users.noreply.github.com>
2025-05-02 00:27:18 -0400

    implement login middleware

--- server/services/middleware.js
+++ server/services/middleware.js
@@ -1,25 +1,87 @@
 import { Readable } from "stream";
-import puppeteer from "puppeteer";
-import { formatObject } from "./logger.js";
-
+import * as client from "openid-client";
+import logger, { formatObject } from "./logger.js";
+const { OAUTH_CALLBACK_URL, OAUTH_DISCOVERY_URL, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET } = process.env;
 export const WHITELIST = [/.*/i];
-export const PROXY_ENDPOINT = "/api/proxy";
+export const oidcConfig = await client.discovery(new URL(OAUTH_DISCOVERY_URL), OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET);
 
+/**
+ * Logs requests
+ * @param {function} formatter 
+ * @returns (request, response, next) => void
+ */
 export function logRequests(formatter = (request) => [request.path]) {
   return (request, response, next) => {
-    const { logger } = request.app.locals;
     request.startTime = new Date().getTime();
     logger.info(formatter(request));
     next();
   };
 }
 
+/**
+ * Logs errors (should be used as the last middleware)
+ * @param {function} formatter 
+ * @returns (error, request, response, next) => void
+ */
 export function logErrors(formatter = (e) => ({ error: e.message })) {
   return (error, request, response, next) => {
-    const { logger } = request.app.locals;
     logger.error(formatObject(error));
     response.status(400).json(formatter(error));
   };
+}
+
+/**
+ * Login middleware for handling OpenID Connect authentication
+ * @param {Express.Request} req
+ * @param {Express.Response} res
+ * @param {Function} next
+ */
+export async function loginMiddleware(req, res, next) {
+  try {
+    const sess = req.session;
+
+    // Initially, we need to build the authorization URL and redirect the user to the authorization server.
+    if (!req.query.code) {
+      sess.destination = req.query.destination || "/";
+      sess.oidc = {
+        codeVerifier: client.randomPKCECodeVerifier(),
+        state: client.randomState(),
+        nonce: client.randomNonce(),
+      };
+
+      const codeChallenge = await client.calculatePKCECodeChallenge(sess.oidc.codeVerifier);
+
+      const authUrl = client.buildAuthorizationUrl(oidcConfig, {
+        response_type: "code",
+        redirect_uri: OAUTH_CALLBACK_URL,
+        scope: "openid profile email",
+        state: sess.oidc.state,
+        nonce: sess.oidc.nonce,
+        code_challenge: codeChallenge,
+        code_challenge_method: "S256",
+        prompt: "login",
+      });
+
+      return res.redirect(authUrl.toString());
+    }
+
+    // After we receive the authorization code, we need to exchange it for an access token.
+    const redirectUrl = new URL(req.originalUrl, `${req.protocol}://${req.get("host")}`);
+    const checks = {
+      expectedState: sess.oidc.state,
+      expectedNonce: sess.oidc.nonce,
+      pkceCodeVerifier: sess.oidc.codeVerifier,
+    };
+    const tokenSet = await client.authorizationCodeGrant(oidcConfig, redirectUrl, checks);
+
+    // Fetch user info using the access token and store it in the session
+    const userinfo = await client.fetchUserInfo(oidcConfig, tokenSet.access_token, tokenSet.claims().sub);
+    sess.tokenSet = tokenSet;
+    sess.userinfo = userinfo;
+    next();
+  } catch (err) {
+    return next(err);
+  }
 }
 
 /**
@@ -31,27 +93,8 @@ export function logErrors(formatter = (e) => ({ error: e.message })) {
 export async function authMiddleware(req, res, next) {
     // todo: switch over when keys are verified
   const authDisabled = true;
-  if (!authDisabled && !req.user) {
+  if (!authDisabled && !req.session.user) {
     return res.status(401).end("Unauthorized");
-  }
-  next();
-}
-
-/**
- * Initializes a Puppeteer browser instance under app.locals
- * @param {Express.Request} req
- * @param {Express.Response} res
- * @param {Function} next
- */
-export async function browserMiddleware(req, res, next) {
-  const { locals } = req.app;
-  if (!locals.browser) {
-    locals.browser = await puppeteer.launch({
-      headless: false,
-      args: ["--no-sandbox", "--disable-setuid-sandbox", "--no-zygote"],
-      protocolTimeout: 240_000,
-    });
-    locals.browser.on("disconnected", () => (locals.browser = null));
   }
   next();
 }
@@ -68,31 +111,11 @@ export async function browserMiddleware(req, res, next) {
 export async function proxyMiddleware(req, res, next) {
   const { headers, method, body } = req;
   const host = headers.host?.split(":")[0];
-
-  // Extract URL from path parameters
-  let urlString = "";
-  let params = { ...req.params, ...req.query, ...req.body };
-  if (params && params.url) {
-    urlString = Array.isArray(params.url) ? params.url.filter(Boolean).join("/") : params.url;
-
-    // Add protocol if missing
-    if (!/^https?:\/\//i.test(urlString)) {
-      urlString = "https://" + urlString;
-    }
+  let urlString = req.path.replace(/^\/[^\/]+\/?/, ""); // remove path prefix
+  if (!/^https?:\/\//i.test(urlString)) {
+    urlString = "https://" + urlString;
   }
-
-  if (!urlString) {
-    res.status(400).send("Bad Request: No URL provided");
-    return;
-  }
-
-  let url;
-  try {
-    url = new URL(urlString);
-  } catch (error) {
-    res.status(400).send(`Invalid URL: ${error.message}`);
-    return;
-  }
+  let url = new URL(urlString)
 
   // Only allow requests if the hostname matches or is on the whitelist
   if (!WHITELIST.some((regex) => regex.test(url.hostname)) && url.hostname !== host) {
@@ -102,7 +125,7 @@ export async function proxyMiddleware(req, res, next) {
 
   try {
     // remove problematic headers
-    const cleanHeaders = { ...headers };
+    const cleanHeaders = { ...headers, ...getAuthorizedHeaders(url) };
     ["host", "connection", "content-length"].forEach((h) => delete cleanHeaders[h]);
     const response = await fetch(getAuthorizedUrl(url), {
       method,
@@ -124,10 +147,18 @@ export async function proxyMiddleware(req, res, next) {
 }
 
 export function getAuthorizedUrl(url, env = process.env) {
-  const key = {
-    "api.govinfo.gov": env.DATA_GOV_API_KEY,
-    "api.congress.gov": env.CONGRESS_GOV_API_KEY,
-  }[url.hostname];
-  key && url.searchParams.append("api_key", key);
+  const params = {
+    "api.govinfo.gov": {api_key: env.DATA_GOV_API_KEY},
+    "api.congress.gov": {api_key: env.CONGRESS_GOV_API_KEY},
+  }[url.hostname] || {};
+  for (const key in params) {
+    url.searchParams.set(key, params[key]);
+  }
   return url.toString();
+}
+
+export function getAuthorizedHeaders(url, env = process.env) {
+  return {
+    "api.search.brave.com": {"x-subscription-token": env.BRAVE_SEARCH_API_KEY},
+  }[url.hostname] || {};
 }
