e5ecadb6c6bce07ac626dd4b4052aef7cc8d3f2a
Brian Park <park-brian@users.noreply.github.com>
2025-05-14 15:11:54 -0400

    adjust spacing

--- client/pages/home.js
+++ client/pages/home.js
@@ -30,41 +30,39 @@ export default function Page() {
   ];
 
   return html`
-    <main>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8">
-            <div class="py-5 d-flex flex-column justify-content-center h-100">
-              <h1 class="text-gradient display-3 fw-semibold mb-4">Welcome to Research Optimizer</h1>
-              <div class="fs-5 pe-3">
-                <p class="mb-3">
-                  The Research Optimizer is a suite of integrated tools designed to address operational and documentation challenges across
-                  every stage of the clinical trial lifecycle. By reducing administrative burden, the platform empowers researchers to
-                  concentrate on scientific discovery and innovation.
-                </p>
-                <p class="mb-3">Designed by clinical research professionals for clinical research professionals.</p>
-                <p class="mb-3">National Cancer Institute - Center for Biomedical Informatics and Information Technology</p>
-                <a class="btn btn-primary btn-lg rounded-pill text-decoration-none" href="/api/login" target="_self">Login</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="py-5 d-flex flex-column justify-content-center h-100">
-              <${For} each=${links}
-                >${(link) => html`
-                  <div class="d-flex align-items-center my-3">
-                    <a href="${link.href}" class="p-2">${link.icon}</a>
-                    <div class="border-start p-2">
-                      <a class="text-decoration-none fs-5" href="${link.href}"> ${link.title} </a>
-                      <div class="text-secondary">${link.description}</div>
-                    </div>
-                  </div>
-                `}
-              <//>
+    <div class="container h-100 d-flex flex-column justify-content-center">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="py-3 d-flex flex-column justify-content-center h-100">
+            <h1 class="text-gradient display-3 fw-semibold mb-4">Welcome to Research Optimizer</h1>
+            <div class="fs-5 pe-3">
+              <p class="mb-3">
+                The Research Optimizer is a suite of integrated tools designed to address operational and documentation challenges across
+                every stage of the clinical trial lifecycle. By reducing administrative burden, the platform empowers researchers to
+                concentrate on scientific discovery and innovation.
+              </p>
+              <p class="mb-3">Designed by clinical research professionals for clinical research professionals.</p>
+              <p class="mb-3">National Cancer Institute - Center for Biomedical Informatics and Information Technology</p>
+              <a class="btn btn-primary btn-lg rounded-pill text-decoration-none" href="/api/login" target="_self">Login</a>
             </div>
           </div>
         </div>
+        <div class="col-lg-4">
+          <div class="py-3 d-flex flex-column justify-content-center h-100">
+            <${For} each=${links}>
+              ${(link) => html`
+                <div class="d-flex align-items-center my-3">
+                  <a href="${link.href}" class="p-2">${link.icon}</a>
+                  <div class="border-start p-2">
+                    <a class="text-decoration-none fs-5" href="${link.href}"> ${link.title} </a>
+                    <div class="text-secondary">${link.description}</div>
+                  </div>
+                </div>
+              `}
+            <//>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   `;
 }
