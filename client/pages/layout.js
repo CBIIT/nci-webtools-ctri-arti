45a67408fb489646e242c017001e4045ac5b237c
huuaho <32457341+huuaho@users.noreply.github.com>
2025-05-20 00:59:07 -0400

    added privacy notice

--- client/pages/layout.js
+++ client/pages/layout.js
@@ -2,6 +2,7 @@ import html from "solid-js/html";
 
 
 import Header from "../components/header.js";
+import PrivacyNotice from "../components/privacy-notice.js";
 import Footer from "../components/footer.js";
 import Nav from "../components/nav.js";
 import routes from "./routes.js";
@@ -12,6 +13,7 @@ export default function Layout({ children }) {
       <${Header} /> 
       <${Nav} routes=${routes} />
     </div>
+    <${PrivacyNotice}/>
     <main class="d-flex flex-column flex-grow-1">${children}</main>
     <${Footer} />
   `;
