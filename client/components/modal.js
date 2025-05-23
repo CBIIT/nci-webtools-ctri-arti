92e058e86648030cdc7623e513f517140370482d
Brian Park <park-brian@users.noreply.github.com>
2025-05-23 15:20:49 -0400

    update modal dialog

--- client/components/modal.js
+++ client/components/modal.js
@@ -1,5 +1,6 @@
-import { createEffect } from "solid-js";
+import { Show, createEffect, createResource } from "solid-js";
 import html from "solid-js/html";
+import { getMarked } from "../utils/utils.js";
 
 /**
  * Modal component for displaying a modal dialog
@@ -8,24 +9,23 @@ import html from "solid-js/html";
  * @param {boolean} props.open - Whether the modal is open or not
  * @param {function} props.setOpen - Function to set the modal open state
  * @param {string} props.title - Title of the modal
- * @param {function} props.onClose - Function to call when the modal is closed
+ * @param {string} props.url - URL to fetch markdown content from
  * @param {function} props.onSubmit - Function to call when the modal is submitted
  * @param {function} props.children - Content of the modal
  * @returns 
  */
 export default function Modal(props) {
-  createEffect(() => (document.body.style.overflow = props.open() ? "hidden" : "auto"));
+  createEffect(() => (document.body.style.overflow = props.open ? "hidden" : "auto"));
+  const [innerHTML] = createResource(() => fetch(props.url).then(r => r.text()).then(getMarked().parse));
   return html`
-    <dialog class="modal border-0 show" open=${props.open} onClose=${(e) => props.setOpen(false)} onSubmit=${props.onSubmit}>
-      <form method="dialog" class="modal-dialog">
+    <dialog class="modal border-0 show" open=${props.open} onClose=${(e) => props.setOpen?.(false)} onSubmit=${props.onSubmit}>
+      <form method="dialog" class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5">${props.title}</h1>
-            <button type="submit" class="btn-close" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">${props.children}</div>
-          <div class="modal-footer">
-            <button type="submit" class="btn btn-primary">Continue</button>
+          <div class="modal-header">${props.title}</div>
+          <div class="modal-body">
+            <${Show} when=${props.url} fallback=${props.children}>
+              <div class="markdown small" innerHTML=${innerHTML} />
+            <//>
           </div>
         </div>
       </form>
