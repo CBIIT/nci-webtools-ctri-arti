ea3b1fa349d2c48b5fab97be09173cb25e927901
Brian Park <park-brian@users.noreply.github.com>
2025-06-08 00:14:00 -0400

    avoid destructuring props

--- client/pages/layout.js
+++ client/pages/layout.js
@@ -7,14 +7,14 @@ import Footer from "../components/footer.js";
 import Nav from "../components/nav.js";
 import routes from "./routes.js";
 
-export default function Layout({ children }) {
+export default function Layout(props) {
   return html`
     <div class="shadow">
       <${Header} /> 
       <${Nav} routes=${routes} />
     </div>
     <${PrivacyNotice}/>
-    <main class="d-flex flex-column flex-grow-1">${children}</main>
+    <main class="d-flex flex-column flex-grow-1">${props.children}</main>
     <${Footer} />
   `;
 }
