082d97f0174fa8a423af3538126cda211db17f41
Brian Park <park-brian@users.noreply.github.com>
2025-06-08 16:33:58 -0400

    use dynamic routes

--- client/pages/routes.js
+++ client/pages/routes.js
@@ -10,86 +10,95 @@ const Users = AuthorizedImport({ path: "./users/index.js", roles: [1] });
 const UserEdit = AuthorizedImport({ path: "./users/edit.js", roles: [1] });
 const UserUsage = AuthorizedImport({ path: "./users/usage.js", roles: [1] });
 
+const { user } = await fetch("/api/session").then(res => res.json());
+const hasRole = (roleIds) => user && roleIds.includes(user?.Role?.id);
+
 const routes = [
-  {
-    path: "",
-    title: "Home",
-    component: Home,
-    hidden: false,
-  },
-  {
-    path: "*",
-    title: "Home",
-    component: Home,
-    hidden: true,
-  },
-  {
-    path: "/tools",
-    title: "Tools",
-    children: [
-      {
-        path: "chat",
-        title: "Chat",
-        component: Chat,
-      },
-      {
-        path: "fedpulse",
-        title: "FedPulse",
-        component: FedPulse,
-      },
-      {
-        path: "consentcrafter",
-        title: "ConsentCrafter",
-        component: ConsentCrafter,
-      },
-      {
-        path: "laypersonabstract",
-        title: "Lay Person Abstract",
-        component: LayPersonAbstract,
-      },
-      {
-        path: "translate",
-        title: "Translate",
-        component: Translate,
-        hidden: true,
-      },
-      {
-        path: "semanticsearch",
-        title: "Semantic Search",
-        component: SemanticSearch,
-        hidden: true,
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    // component: Home,
-    title: "Admin",
-    children: [
-      {
-        path: "users",
-        title: "Users",
-        component: Users,
-      },
-      {
-        path: "users/:id",
-        title: "User",
-        component: UserEdit,
-        hidden: true,
-      },
-      {
-        path: "usage",
-        title: "Usage",
-        component: Users,
-      },
-      {
-        path: "users/:id/usage",
-        title: "User Usage",
-        component: UserUsage,
-        hidden: true,
-      },
-    ],
-  }
-];
+    {
+      path: "",
+      title: "Home",
+      component: Home,
+      hidden: false,
+    },
+    {
+      path: "*",
+      title: "Home",
+      component: Home,
+      hidden: true,
+    },
+    {
+      path: "/tools",
+      title: "Tools",
+      children: [
+        {
+          path: "chat",
+          title: "Chat",
+          component: Chat,
+        },
+        {
+          path: "fedpulse",
+          title: "FedPulse",
+          component: FedPulse,
+        },
+        {
+          path: "consentcrafter",
+          title: "ConsentCrafter",
+          component: ConsentCrafter,
+        },
+        {
+          path: "laypersonabstract",
+          title: "Lay Person Abstract",
+          component: LayPersonAbstract,
+        },
+        {
+          path: "translate",
+          title: "Translate",
+          component: Translate,
+          hidden: !hasRole([1,2]),
+        },
+        {
+          path: "semanticsearch",
+          title: "Semantic Search",
+          component: SemanticSearch,
+          hidden: !hasRole([1,2]),
+        },
+      ],
+    },
+    {
+      path: "/_",
+      rawPath: !user ? "/api/login" : undefined,
+      title: user?.firstName || "Login",
+      children: user && [
+        {
+          path: "users",
+          title: "Users",
+          component: Users,
+          hidden: !hasRole([1]),
+        },
+        {
+          path: "users/:id",
+          title: "User",
+          component: UserEdit,
+          hidden: true,
+        },
+        {
+          path: "usage",
+          title: "Usage",
+          component: Users,
+          hidden: !hasRole([1]),
+        },
+        {
+          path: "users/:id/usage",
+          title: "User Usage",
+          component: UserUsage,
+          hidden: true,
+        },
+        {
+          rawPath: "/api/logout",
+          title: "Logout",
+        }
+      ],
+    }
+  ];
 
 export default routes;
