5857ed3e49e5c30ce6936f811289592d585e4cbe
Brian Park <park-brian@users.noreply.github.com>
2025-06-08 16:33:19 -0400

    remove redundant nav components

--- client/components/nav.js
+++ client/components/nav.js
@@ -2,10 +2,10 @@ import html from "solid-js/html";
 import { A } from "@solidjs/router";
 import { createSignal, createResource, For, Show } from "solid-js";
 
-export default function Nav({ routes }) {
+export default function Nav(props) {
   const [session] = createResource(() => fetch("/api/session").then((res) => res.json()));
   const [visible, setVisible] = createSignal({});
-  const toggleVisible = (key) => setVisible((prev) => ({ ...prev, [key]: !prev[key] }));
+  const toggleVisible = (key) => setVisible((prev) => ({ [key]: !prev[key] }));
   return html`
     <nav class="navbar navbar-expand-lg font-title">
       <div class="container">
@@ -23,13 +23,14 @@ export default function Nav({ routes }) {
         </button>
         <div id="navbar" class="collapse navbar-collapse" classList=${() => ({ show: visible().main })}>
           <ul class="navbar-nav me-auto">
-            <${For} each=${() => routes.filter((route) => !route.hidden)}>
+            <${For} each=${() => props.routes.filter((route) => !route.hidden)}>
               ${(route) => html`
                 <li class="nav-item" classList=${{ dropdown: route.children }}>
                   <${A}
-                    href=${route.path}
+                    href=${route.rawPath || route.path}
                     end=${true}
                     activeClass="active"
+                    target=${() => route.rawPath && "_self"}
                     class="nav-link text-decoration-none"
                     classList=${{ "dropdown-toggle": route.children }}
                     onClick=${(ev) => (route.children ? (ev.preventDefault(), toggleVisible(route.path)) : setVisible({}))}>
@@ -41,8 +42,9 @@ export default function Nav({ routes }) {
                         ${(child) => html`
                           <li>
                             <${A}
-                              href=${[route.path, child.path].join("/")}
+                              href=${child.rawPath || [route.path, child.path].join("/")}
                               activeClass="active"
+                              target=${child.rawPath && "_self" }
                               class="dropdown-item nav-link text-decoration-none"
                               onClick=${() => setVisible({})}>
                               ${child.title}
@@ -56,46 +58,6 @@ export default function Nav({ routes }) {
               `}
             <//>
           </ul>
-          <div class="navbar-nav">
-            <${Show}
-              when=${() => session()?.user}
-              fallback=${html`<a href="/api/login" target="_self" class="nav-link text-decoration-none">Login</a>`}>
-              <li class="nav-item dropdown login" classList=${() => ({ "bg-info": visible().user })}>
-                <button class="nav-link dropdown-toggle" onClick=${() => toggleVisible("user")} classList=${() => ({ "text-light": visible().user })}>
-                  ${() => session()?.user?.firstName || "User"}
-                </button>
-                <div class="dropdown-menu border-0 rounded-0 bg-info p-4" classList=${() => ({ show: visible().user })}>
-                  <div class="container">
-                    <div class="d-flex flex-row gap-4 px-2">
-                      <div>
-                        <${A}
-                          href=${() => `/user/profile/${session()?.user?.id}`}
-                          class="dropdown-item nav-link text-decoration-none text-light border-transparent"
-                          onClick=${() => setVisible({})}>
-                          User Profile
-                        <//>
-                      </div>
-                      <${For} each=${() => routes.filter((r) => r.loginNavbar && r.loginNavbarTitle && r.allowedRoles?.includes(session()?.user?.roleId))}>
-                        ${(route) => html`
-                          <div>
-                            <${A}
-                              href=${route.path}
-                              class="dropdown-item nav-link text-decoration-none text-light border-transparent"
-                              onClick=${() => setVisible({})}>
-                              ${route.loginNavbarTitle || route.title}
-                            <//>
-                          </div>
-                        `}
-                      <//>
-                      <div>
-                        <a href="/api/logout" target="_self" class="dropdown-item nav-link text-decoration-none text-light border-transparent">Logout</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            <//>
-          </div>
         </div>
       </div>
     </nav>
