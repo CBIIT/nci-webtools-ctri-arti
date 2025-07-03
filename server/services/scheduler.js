a0fcd2799eb6db2e1f4b0597c7dcb620b91009fc
Brian Park <park-brian@users.noreply.github.com>
2025-07-03 03:22:18 -0400

    add scheduler

---
+++ server/services/scheduler.js
@@ -0,0 +1,9 @@
+import cron from 'node-cron';
+import { User } from './database.js';
+import { Op } from 'sequelize';
+
+const { USAGE_RESET_SCHEDULE = '0 0 * * 0' } = process.env;
+
+export const resetUsageLimits = () => User.update({ remaining: User.sequelize.literal('limit') }, { where: { limit: { [Op.ne]: null } } });
+
+export const startScheduler = () => cron.schedule(USAGE_RESET_SCHEDULE, resetUsageLimits);
