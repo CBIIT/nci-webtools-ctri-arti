183360142cadfef0a80e518cfa53b3e8f705427b
Brian Park <park-brian@users.noreply.github.com>
2025-08-08 02:14:54 -0400

    rearrange chat

--- client/pages/tools/chat/index.js
+++ client/pages/tools/chat/index.js
@@ -1,4 +1,4 @@
-import { createSignal, For, Show, Index } from "solid-js";
+import { createSignal, createResource, For, Show, Index } from "solid-js";
 import html from "solid-js/html";
 import Loader from "/components/loader.js";
 import { AlertContainer } from "/components/alert.js";
@@ -8,6 +8,7 @@ import Message from "./message.js";
 import { alerts, clearAlert } from "/utils/alerts.js";
 
 export default function Page() {
+  const [session] = createResource(() => fetch("/api/session").then(res => res.json()));
   const { conversation, updateConversation, deleteConversation, conversations, messages, loading, submitMessage } = useChat();
   const [toggles, setToggles] = createSignal({});
   const toggle = (key) => () => setToggles((prev) => ({ ...prev, [key]: !prev[key] }));
@@ -34,148 +35,145 @@ export default function Page() {
   }
 
   return html`
-    <div class="container position-relative py-3">
-      <${AlertContainer} alerts=${alerts} onDismiss=${clearAlert} />
-      <div class="row">
-        <div class="col d-flex align-items-center">
-          <button class="btn btn-sm btn-light d-flex align-items-center" onClick=${toggle("conversations")}>
-            <img src="assets/images/icon-history.svg" alt="Conversations" width="16" />
-          </button>
-          <input
-            value=${() => conversation?.title}
-            onChange=${(ev) => updateConversation({ title: ev.target.value })}
-            class="form-control form-control-sm fw-semibold border-0 bg-transparent flex-grow-1" />
-          <${Show} when=${() => conversation?.id}>
-            <button 
-              class="btn btn-sm btn-outline-danger ms-2" 
-              onClick=${deleteConversation}
-              title="Delete conversation">
-              Delete
+    <div class="row">
+      <div class="col-sm-auto">
+        <${Show} when=${() => !toggles().conversations}>
+          <div class="d-flex p-3 position-absolute z-3">
+            <button type="button" class="btn btn-sm px-0" onClick=${toggle("conversations")}>
+              <img src="assets/images/icon-history.svg" alt="Conversations" width="16" />
             </button>
-          <//>
-        </div>
-      </div>
-    </div>
-
-
-    <aside
-      class=${() => ["offcanvas offcanvas-start", toggles().conversations ? "show" : "hiding"].join(" ")}
-      tabindex="-1"
-      id="conversations-menu"
-      aria-labelledby="conversations-menu-label">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="conversations-menu-label">Conversations</h5>
-        <button type="button" class="btn-close" aria-label="Close" onClick=${toggle("conversations")}></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/tools/chat" target="_blank">New Conversation</a>
-          </li>
-          <${For} each=${conversations}>
-            ${(conv) =>
-              html`<li class="nav-item">
-                <a
-                  class="nav-link"
-                  href=${`/tools/chat?id=${conv.id}`}
-                  target="_self"
-                  classList=${() => ({ active: conv.id === conversation?.id })}>
-                  ${conv.title}
-                </a>
-              </li>`}
-          <//>
-        </ul>
-      </div>
-    </aside>
-
-    <main class="container d-flex flex-column flex-grow-1 mb-3 position-relative">
-      <div class="flex-grow-1 py-3" classList=${() => ({ "x-mvh-100": messages.length > 0 })}>
-        <div class="text-center my-5 font-serif" hidden=${() => messages.length > 0}>
-          <h1 class="text-gradient fw-bold font-title mb-2">Welcome</h1>
-          <div class="text-secondary fw-semibold">How can we help you today?</div>
-        </div>
-        <${Index} each=${messages}>
-          ${(message, index) => html`
-            <${Message}
-              message=${message}
-              index=${index}
-              messages=${messages}
-              class="small markdown shadow-sm rounded mb-3 p-2 position-relative" />
-          `}
-        <//>
-        <${Show} when=${loading}><${Loader} style="display: block; height: 1.1rem; width: 100%; margin: 1rem 0; opacity: 0.5" /><//>
-      </div>
-      <div class="position-sticky bottom-0 bg-white">
-        <div class="small d-flex justify-content-between">
-          <div class="d-flex align-items-center">
-            Export as
-            <button
-              class="btn btn-sm p-0 btn-link mx-1"
-              onClick=${() =>
-                downloadCsv(
-                  "conversation.csv",
-                  messages.map((m) => ({
-                    role: m.role,
-                    content: m.content
-                      ?.map((c) => c.text)
-                      .filter(Boolean)
-                      .map(e => e.trim())
-                      .join("\n"),
-                  }))
-                )}>
-              csv
-            </button>
-            or
-            <button class="btn btn-sm p-0 btn-link mx-1" onClick=${() => downloadJson("conversation.json", messages)}>json</button>
           </div>
-          <a href="/tools/chat" target="_blank">Start a new conversation</a>
-        </div>
-        <form onSubmit=${handleSubmit} class="bg-light shadow-sm rounded">
-          <textarea
-            class="form-control form-control-sm border-0 bg-transparent shadow-0"
-            onKeyDown=${handleKeyDown}
-            id="message"
-            name="message"
-            placeholder="Enter message (Shift + Enter for new line)"
-            rows="3"
-            autofocus
-            required />
+        <//>
 
-          <div class="d-flex justify-content-between">
-            <input
-              type="file"
-              id="inputFiles"
-              name="inputFiles"
-              class="form-control form-control-sm w-auto bg-transparent border-transparent"
-              accept="image/*,text/*,.pdf,.xls,.xlsx"
-              multiple />
-
-            <div class="input-group w-auto align-items-center">
-              <div class="form-check form-switch form form-check-reverse form-control-sm">
-                <input class="form-check-input" type="checkbox" id="reasoningMode" name="reasoningMode" />
-                <label
-                  class="form-check-label text-secondary"
-                  for="reasoningMode"
-                  title="Enable this mode for more thorough responses to complex problems. Please note this requires additional time and resources.">
-                  Extended Thinking Mode
-                </label>
+        <aside
+          class=${() => ["position-relative z-5 bg-light p-3", toggles().conversations ? "d-block" : "d-none"].join(" ")}
+          tabindex="-1"
+          id="conversations-menu"
+          aria-labelledby="conversations-menu-label">
+          <div class="d-flex mb-3">
+            <button type="button" class="btn btn-sm px-0" onClick=${toggle("conversations")}>
+              <img src="assets/images/icon-history.svg" alt="Conversations" width="16" />
+            </button>          
+          </div>
+          <ul class="list-unstyled">
+            <li class="mb-3">
+              <a class="text-muted" href="/tools/chat" target="_blank">New Chat</a>
+            </li>
+            <li class="mb-2 fw-bold text-secondary">
+              Recent
+            </li>
+            <${For} each=${conversations}>
+              ${(conv) =>
+                html`<li class="mb-2">
+                  <a
+                    class="link-primary text-decoration-none"
+                    href=${`/tools/chat?id=${conv.id}`}
+                    target="_self"
+                    classList=${() => ({ active: conv.id === conversation?.id })}>
+                    ${conv.title}
+                  </a>
+                </li>`}
+            <//>
+          </ul>
+        </aside>
+      </div>
+      <div class="col-sm">
+        <form onSubmit=${handleSubmit} class="container d-flex flex-column flex-grow-1 mb-3 position-relative">
+          <${AlertContainer} alerts=${alerts} onDismiss=${clearAlert} />
+          <div class="row">
+            <div class="col d-flex align-items-center justify-content-between py-3">
+              <div class="d-flex align-items-center">
+                <div class="form-check form-switch form form-check-reverse form-control-sm my-0 me-2">
+                  <input class="form-check-input" type="checkbox" id="fedpulse" name="fedpulse" />
+                  <label
+                    class="form-check-label text-secondary"
+                    for="fedpulse"
+                    title="Enable FedPulse">
+                    FedPulse
+                  </label>
+                </div>
+                <select class="form-select form-select-sm border-0 bg-light cursor-pointer" name="model" id="model" required>
+                  <option value="us.anthropic.claude-opus-4-1-20250805-v1:0">Opus 4.1</option>
+                  <option value="us.anthropic.claude-sonnet-4-20250514-v1:0" selected>Sonnet 4.0</option>
+                  <option value="us.anthropic.claude-3-5-haiku-20241022-v1:0">Haiku 3.5</option>
+                </select>
               </div>
-              <select class="form-select form-select-sm border-0 bg-transparent cursor-pointer" name="model" id="model" required>
-                <option value="us.anthropic.claude-opus-4-1-20250805-v1:0">Opus</option>
-                <option value="us.anthropic.claude-sonnet-4-20250514-v1:0" selected>Sonnet</option>
-                <option value="us.anthropic.claude-3-5-haiku-20241022-v1:0">Haiku</option>
-              </select>
-              <button class="btn btn-dark btn-sm ms-2" type="submit" style="border-radius: 0 0 var(--bs-border-radius-sm) 0">Send</button>
+
+              <${Show} when=${() => conversation?.id}>
+                <button 
+                  type="button"
+                  class="btn btn-sm btn-outline-danger ms-2" 
+                  onClick=${deleteConversation}
+                  title="Delete conversation">
+                  Delete
+                </button>
+              <//>
+            </div>
+          </div>
+
+          <div class="flex-grow-1 py-3" classList=${() => ({ "x-mvh-100": messages.length > 0 })}>
+            <div class="text-center my-5 font-serif" hidden=${() => messages.length > 0}>
+              <h1 class="text-gradient fw-bold font-title mb-2">Welcome, ${() => session()?.user?.firstName || ''}</h1>
+              <div class="text-secondary fw-semibold">How can I help you today?</div>
+            </div>
+            <${Index} each=${messages}>
+              ${(message, index) => html`
+                <${Message}
+                  message=${message}
+                  index=${index}
+                  messages=${messages}
+                  class="small markdown shadow-sm rounded mb-3 p-2 position-relative" />
+              `}
+            <//>
+            <${Show} when=${loading}><${Loader} style="display: block; height: 1.1rem; width: 100%; margin: 1rem 0; opacity: 0.5" /><//>
+          </div>
+          <div class="position-sticky bottom-0 bg-white">
+            <div class="bg-light shadow-sm rounded">
+              <textarea
+                class="form-control form-control-sm border-0 bg-transparent shadow-0"
+                onKeyDown=${handleKeyDown}
+                id="message"
+                name="message"
+                placeholder="Ask me (Shift + Enter for new line)"
+                rows="3"
+                autofocus
+                required />
+
+              <div class="d-flex justify-content-between py-1 px-2">
+                <label class="btn btn-light btn-sm rounded-pill">
+                  <input
+                    type="file"
+                    id="inputFiles"
+                    name="inputFiles"
+                    class="visually-hidden"
+                    accept="image/*,text/*,.pdf,.xls,.xlsx"
+                    multiple />
+                    +
+                </label>
+
+                <div class="d-flex w-auto align-items-center">
+                  <div class="form-check form-switch   form-control-sm me-2">
+                    <input class="form-check-input" type="checkbox" id="reasoningMode" name="reasoningMode" />
+                    <label
+                      class="form-check-label text-secondary"
+                      for="reasoningMode"
+                      title="Enable this mode for more thorough responses to complex problems. Please note this requires additional time and resources.">
+                      Research Mode
+                    </label>
+                  </div>
+                  <button class="btn btn-primary btn-sm rounded-pill px-3" type="submit" style="border-radius: 0 0 var(--bs-border-radius-sm) 0">Send</button>
+                </div>  
+              </div>
+            </div>
+            <div class="text-center text-muted small py-1">
+              <span class="me-1" title="Your conversations are stored only on your personal device.">
+                To maintain your privacy, we never retain your data on our systems.
+              </span>
+              Please double-check statements, as Research Optimizer can make mistakes.
             </div>
           </div>
         </form>
-        <div class="text-center text-muted small py-1">
-          <span class="me-1" title="Your conversations are stored only on your personal device.">
-            To maintain your privacy, we never retain your data on our systems.
-          </span>
-          Please double-check statements, as Research Optimizer can make mistakes.
-        </div>
       </div>
-    </main>
+    </div>
   `;
 }
