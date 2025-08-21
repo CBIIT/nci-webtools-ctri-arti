e9eec7cae4eaf8d919547ed01c3525a5fef8cd98
Brian Park <park-brian@users.noreply.github.com>
2025-08-21 00:54:35 -0400

    add message tests

--- client/test/run.js
+++ client/test/run.js
@@ -1,5 +1,9 @@
 // Import tests using the new test framework
 import './solidjs.test.js';
+
+// Page tests
+import './pages/tools/chat/message.test.js';
+
 import { run } from './test.js';
 
 // Run all tests and await for completion before setting TESTS_DONE
