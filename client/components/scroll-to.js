b25a7dc4c056abc6b5461b3918e532481811ec53
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-09-17 17:11:06 -0400

    update scroll-to button to be an icon button

--- client/components/scroll-to.js
+++ client/components/scroll-to.js
@@ -1,5 +1,7 @@
 import html from "solid-js/html";
 
+import { ArrowDown } from "lucide-solid";
+
 /**
  * Scrolls the target element into view.
  *
@@ -27,23 +29,9 @@ export default function ScrollTo(props) {
     <button
       type="button"
       onClick=${scrollToElement}
-      class="btn btn-primary d-flex justify-content-center align-items-center text-nowrap fw-semibold pe-auto gap-2 rounded-pill px-[12px] ps-3 fs-08 focus-ring text-white"
+      class="scroll-down-btn bg-white rounded-full d-flex justify-content-center align-items-center focus-ring shadow-lg"
     >
-      <span class="pb-0">${props.label}</span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="w-1r h-1r"
-      >
-        <path d="m6 9 6 6 6-6" />
-      </svg>
+      <${ArrowDown} size="20" color="black" />
     </button>
   </div>`;
 }
