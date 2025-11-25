fab40715fa1459fa33d5b58fd024b23cf3a1b93c
Brian Park <park-brian@users.noreply.github.com>
2025-11-24 21:53:08 -0500

    support TranslateDocument

--- server/services/translate.js
+++ server/services/translate.js
@@ -2,30 +2,65 @@ import {
   paginateListLanguages,
   TranslateClient,
   TranslateTextCommand,
+  TranslateDocumentCommand,
 } from "@aws-sdk/client-translate";
 
 // ES Modules import
 
 export async function translate({
   text,
-  sourceLanguage = "en",
+  content = null,
+  contentType = "text/plain",
+  sourceLanguage = "auto",
   targetLanguage = "es",
   settings = {},
 }) {
   const client = new TranslateClient();
-  const input = {
-    Text: text,
-    SourceLanguageCode: sourceLanguage,
-    TargetLanguageCode: targetLanguage,
-    Settings: {
-      Formality: "FORMAL",
-      Profanity: "MASK",
-      ...settings,
-    },
-  };
-  const command = new TranslateTextCommand(input);
-  const response = await client.send(command);
-  return response.TranslatedText;
+
+  if (content && contentType) {
+    if (content.includes(";base64,")) {
+      let parts = content.replace("data:", "").split(";base64,");
+      contentType = parts[0];
+      content = parts[1];
+
+      console.log("Detected content type:", contentType);
+      console.log("Content length (base64):", content.length);
+      console.log("Content length (bytes):", Buffer.from(content, "base64").length);
+      console.log("Sample content (base64):", content.slice(0, 100) + "...");
+    }
+    const input = {
+      Document: {
+        Content: Buffer.from(content, "base64"),
+        ContentType: contentType,
+      },
+      SourceLanguageCode: sourceLanguage,
+      TargetLanguageCode: targetLanguage,
+      Settings: {
+        Formality: "FORMAL",
+        Profanity: "MASK",
+        ...settings,
+      },
+    };
+    console.log(input);
+    const command = new TranslateDocumentCommand(input);
+    const response = await client.send(command);
+    const results = response.TranslatedDocument.Content;
+    return "data:" + contentType + ";base64," + Buffer.from(results).toString("base64");
+  } else if (text) {
+    const input = {
+      Text: text,
+      SourceLanguageCode: sourceLanguage,
+      TargetLanguageCode: targetLanguage,
+      Settings: {
+        Formality: "FORMAL",
+        Profanity: "MASK",
+        ...settings,
+      },
+    };
+    const command = new TranslateTextCommand(input);
+    const response = await client.send(command);
+    return response.TranslatedText;
+  }
 }
 
 export async function getLanguages() {
