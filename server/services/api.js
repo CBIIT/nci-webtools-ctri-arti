cd8634982d64d332143979eb7bfb6b5f1caf4bd1
Brian Park <park-brian@users.noreply.github.com>
2025-12-11 02:41:53 -0500

    add conversation api

--- server/services/api.js
+++ server/services/api.js
@@ -3,6 +3,7 @@ import { json, Router } from "express";
 import { logErrors, logRequests } from "./middleware.js";
 import adminRoutes from "./routes/admin.js";
 import authRoutes from "./routes/auth.js";
+import conversationRoutes from "./routes/conversations.js";
 import modelRoutes from "./routes/model.js";
 import toolRoutes from "./routes/tools.js";
 
@@ -12,6 +13,7 @@ api.use(json({ limit: 1024 ** 3 })); // 1GB
 api.use(logRequests());
 api.use(adminRoutes);
 api.use(authRoutes);
+api.use(conversationRoutes);
 api.use(modelRoutes);
 api.use(toolRoutes);
 api.use(logErrors());
